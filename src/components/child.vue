b832dfbb53fcee9077d8dc79e6b4abd5156633bd
ziyang.wen <wenziyangday@gmail.com>
2018-03-28 17:37:20 +0800

    组件库

---
+++ src/components/child.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="child">
+    child
+    <button @click="emitToFather">点击传输给父级组件</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'child',
+    methods: {
+		  emitToFather() {
+		    this.$emit('getEmitToFather', '传输给父级组件的东西');
+      }
+    }
+	}
+</script>
+
+<style lang="stylus">
+  .child
+    font-face-src
+</style>
