9df7d4d8ca0f84452773a5b3e182422a601bb193
ziyang.wen <wenziyangday@gmail.com>
2018-03-28 17:42:03 +0800

    网站登录

---
+++ src/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="login">
+ 
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'login'
+	}
+</script>
+
+<style lang="stylus" type="text/stylus">
+  .login
+    width 100%
+    background url(../assets/images/login.jpg) center no-repeat
+    background-size cover
+</style>
