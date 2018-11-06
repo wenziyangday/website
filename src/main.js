7a972ea27c52a14cc651e2422de1087658d327e8
闻紫阳 <wenzy@orderhandler.com>
2018-11-06 17:38:21 +0800

    引入iconfont进入项目

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import store from './store/store';
 import axios from 'axios';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './font/iconfont.css';
 import echarts from 'echarts';
 import util from './util/util';
 
